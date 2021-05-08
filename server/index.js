51420fd9c628df02d6f133fd93fa5abc683d9658
Samarth Gowda <samarthgowda@Samarths-MBP.fios-router.home>
2021-05-08 18:55:11 -0400

    server && mongoDB Setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+
+import posts from './routes/posts.js'
+
+const app = express()
+
+app.use('/posts',posts)
+
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(cors())
+
+const CONNECTION_URL='mongodb+srv://samarthgowda:test123@cluster0.jippb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT= process.env.PORT||5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(() => app.listen(PORT,()=>console.log(`Server running on PORT: ${PORT}`)))
+.catch((err)=>console.log(err.message))
+
+mongoose.set('useFindAndModify',false);

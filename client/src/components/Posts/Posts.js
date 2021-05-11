df19ab9b20301a1092d245d5dc188540f1f21bfa
Samarth Gowda <samarthgowda@Samarths-MBP.fios-router.home>
2021-05-10 21:09:43 -0400

    Posting and Displaying the posted Data

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Post from './Post/Post'
 import useStyles from './Styles'
+import {Grid, CircularProgress} from '@material-ui/core'
 import {useSelector} from 'react-redux'
 const Posts=() => {
     const posts= useSelector((state)=>state.posts);
@@ -8,11 +9,17 @@ const Posts=() => {
     console.log(posts)
 
     return(
-        <>
-        <h1>Posts</h1>
-        <Post/>
-        <Post/>
-        </>
+        !posts.length? <CircularProgress/>:(
+            <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+                {posts.map((post)=>(
+                    <Grid key={post._id} item xs={12} md={6}>
+                        <Post post={post}/>
+
+                    </Grid>
+
+                ))}
+            </Grid>
+        )
 
     )
 }
